a82f4066d72362be570aed9421c92544ce7dc509
Dave Irwin <david@redirwin.com>
2019-06-21 08:48:07 -0700

    Set up Fetch reducers and axios get request.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,25 @@
+import axios from "axios";
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+
+export const FETCH_SMURFS_START = "FETCH_SMURFS_START";
+export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
+export const FETCH_SMURFS_FAILURE = "FETCH_SMURFS_FAILURE";
+
+export const getSmurfList = () => dispatch => {
+  dispatch({ type: FETCH_SMURFS_START });
+  return axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => {
+      dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res });
+    })
+    .catch(err => {
+      dispatch({ type: FETCH_SMURFS_FAILURE, payload: err });
+    });
+};
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
