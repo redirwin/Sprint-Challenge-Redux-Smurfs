2d70c3cf6396aec3b85b6537b91f27f7199038da
Dave Irwin <david@redirwin.com>
2019-06-21 08:51:45 -0700

    Added export to reducer.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -31,3 +31,5 @@ const reducer = (state = initialState, action) => {
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default reducer;
