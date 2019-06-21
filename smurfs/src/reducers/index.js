b593d04846f4f7d7a8b289d0af7e72c3c910260c
Dave Irwin <david@redirwin.com>
2019-06-21 08:56:28 -0700

    Added switch cases.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -21,6 +21,31 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_SMURFS_START:
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        error: false
+      };
+
+    case FETCH_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload.data,
+        fetchingSmurfs: false,
+        error: false
+      };
+
+    case FETCH_SMURFS_FAILURE:
+      return {
+        ...state,
+        smurfs: [],
+        fetchingSmurfs: false,
+        error: action.payload
+      };
+
+    default:
+      return state;
   }
 };
 
